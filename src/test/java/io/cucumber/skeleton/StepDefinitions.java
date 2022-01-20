5a28bba6c5f2bd6f85e2af5802d5198fd4fba4ea
Eric de Graaf <egraaf@thinkpad.local>
2022-01-20 21:07:52 +0100

    Remove old feature

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -20,23 +20,6 @@ public class StepDefinitions {
     private final Map<String, Float> southEuropeCityTemp = new HashMap<>();
 
 
-    @Given("I have {int} cukes in my belly")
-    public void I_have_cukes_in_my_belly(int cukes) {
-        Belly belly = new Belly();
-        belly.eat(cukes);
-    }
-
-    @When("I wait {int} hour")
-    public void i_wait_hour(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("my belly should growl")
-    public void my_belly_should_growl() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
     @Given("that i'm in a city in The Netherlands")
     public void that_i_m_in_a_city_in_the_netherlands() {
         System.out.println("Hello FOSDEM! ");
